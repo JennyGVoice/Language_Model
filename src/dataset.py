de30bbacccbfbe1ecd316ef296fcf64a6987453d
JennyGVoice <2248571663@qq.com>
2025-12-09 19:04:39 +0100

    update tokenizer

---
+++ src/dataset.py
@@ -0,0 +1,37 @@
+import torch
+from torch.utils.data import Dataset
+
+class CharDataset(Dataset):
+    """
+    PyTorch Dataset for training and evaluating a character-level GPT model.
+    """
+
+    def __init__(self, text, block_size=128, split="train"):
+        self.block_size = block_size
+
+        # Build vocabulary
+        chars = sorted(list(set(text)))
+        self.stoi = {ch: i for i, ch in enumerate(chars)}
+        self.itos = {i: ch for ch, i in self.stoi.items()}
+        self.vocab_size = len(chars)
+
+        # Encode all text
+        ids = torch.tensor([self.stoi[c] for c in text], dtype=torch.long)
+
+        split_idx = int(0.9 * len(ids))
+        if split == "train":
+            self.data = ids[:split_idx]
+        else:
+            self.data = ids[split_idx:]
+
+    def __len__(self):
+        return len(self.data) - self.block_size
+
+    def __getitem__(self, idx):
+        chunk = self.data[idx : idx + self.block_size + 1]
+        x = chunk[:-1]
+        y = chunk[1:]
+        return x, y
+
+    def decode(self, ids):
+        return "".join([self.itos[i] for i in ids])
